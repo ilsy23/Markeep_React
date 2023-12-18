89835d71d39e6b00cd7286330ac9e8789d2df067
ilsy23 <ilsysli@naver.com>
2023-12-18 19:13:47 +0900

    네이버 로그인 1차 수정

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header";
 import NaverLogin from "./components/login/NaverLogin";
+import { AuthContextProvider } from "./utils/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/oauth/naver" element={<NaverLogin />} />
-      </Routes>
-    </div>
+    <AuthContextProvider>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/oauth/naver" element={<NaverLogin />} />
+        </Routes>
+      </div>
+    </AuthContextProvider>
   );
 }
 
--- src/components/login/NaverLogin.js
+++ src/components/login/NaverLogin.js
@@ -5,19 +5,21 @@ import { useNavigate } from "react-router-dom";
 
 const NaverLogin = () => {
   const code = new URL(window.location.href).searchParams.get("code");
+  console.log(code);
   const { onLogin } = useContext(AuthContext);
   const redirection = useNavigate();
+  const API_REQUEST_URL = API_BASE_URL + USER + "/naver-login?code=" + code;
+  console.log(API_REQUEST_URL);
 
   useEffect(() => {
     const naverLogin = async () => {
-      const res = await fetch(
-        API_BASE_URL + USER + "/naver-login?code=" + code
-      );
+      console.log("요청!!!");
+      const res = await fetch(API_REQUEST_URL);
 
       const { token, nickName, email } = await res.json();
       onLogin(token, nickName, email);
-      redirection("/");
     };
+    naverLogin();
   }, []);
 
   return <div>NaverLogin</div>;
