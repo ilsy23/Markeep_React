a65e310e1098a09e8d0c45f199866ca8353884f0
ilsy23 <ilsysli@naver.com>
2023-12-26 10:21:54 +0900

    폴더 검색 기초

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Community from './pages/Community';
-import MyPage from './pages/MyPage';
-import Search from './pages/Search';
-import Detail from './pages/Detail';
-import './styles/App.scss';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Community from "./pages/Community";
+import MyPage from "./pages/MyPage";
+import Search from "./pages/Search";
+import Detail from "./pages/Detail";
+import "./styles/App.scss";
 
 function App() {
   return (
-    <div className='app'>
-      <div className='nav'>
+    <div className="app">
+      <div className="nav">
         <Nav />
       </div>
-      <div className='nav-modal'></div>
-      <div className='main-wrapper'>
-        <div className='header'>
+      <div className="nav-modal"></div>
+      <div className="main-wrapper">
+        <div className="header">
           <Header />
         </div>
-        <div className='content-wrapper'>
+        <div className="content-wrapper">
           <Routes>
-            <Route
-              exact
-              path='/'
-              element={<Community />}
-            />
-            <Route
-              path='/mypage'
-              element={<MyPage />}
-            />
-            <Route
-              path='/search/:keyword'
-              element={<Search />}
-            />
-            <Route
-              path='/detail'
-              element={<Detail />}
-            />
+            <Route exact path="/" element={<Community />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/search/:keyword" element={<Search />} />
+            <Route path="/detail/:folderId" element={<Detail />} />
           </Routes>
         </div>
       </div>
--- src/components/SearchFolder.js
+++ src/components/SearchFolder.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { ReactComponent as SearchIcon } from "../assets/icons/search.svg";
+import Input from "./Input";
+import styles from "../styles/SearchFolder.module.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 const SearchFolder = () => {
-  return <div>SearchFolder</div>;
+  const [searchInput, setSearchInput] = useState("");
+  const inputRef = useRef();
+
+  const getSearchData = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const keyDownHandler = (e) => {
+    if (e.key === "Enter") {
+      clickSearchHandler();
+      inputRef.current.blur();
+    }
+  };
+
+  const clickSearchHandler = () => {
+    // 검색어에 해당하는 내 폴더 목록 불러오기
+    //           <Link to={`/detail/${folderId}`}>폴더이름</Link>
+  };
+
+  return (
+    <>
+      <div className={styles.search}>
+        <Input>
+          <input
+            type="text"
+            placeholder="내 폴더 검색"
+            value={searchInput}
+            onChange={getSearchData}
+            onKeyDown={keyDownHandler}
+            ref={inputRef}
+          />
+          <div className={styles.icon_box} onClick={clickSearchHandler}>
+            <SearchIcon className={styles.icon} />
+          </div>
+        </Input>
+      </div>
+      <ul>
+        <li>
+          <Link to={`/detail`}>폴더 이름입니다. 폴더폴더폴더폴더폴더</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default SearchFolder;
